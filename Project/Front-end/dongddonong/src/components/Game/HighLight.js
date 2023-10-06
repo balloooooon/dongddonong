89b28587a1a1826d733124edfd983d94aa4912ca
이동주 <leednj0113@naver.com>
2023-10-06 09:29:19 +0900

    Update HighLight.js

--- Project/Front-end/dongddonong/src/components/Game/HighLight.js
+++ Project/Front-end/dongddonong/src/components/Game/HighLight.js
@@ -36,7 +36,7 @@ const HighLight = (props) => {
       />
       <ReactPlayer
         // "https://dongddonong.s3.ap-northeast-2.amazonaws.com/1266.mp4"
-        url={props.videoURL}
+        url="https://www.youtube.com/shorts/A0qQEU0W3jE"
         controls
         ref={videoRef}
         width={"100%"}
